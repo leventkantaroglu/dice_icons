78c3210bf9bbc8936c81acaf24fe2016f920bc4b
Levent Kantaroğlu <63751824+leventkantaroglu@users.noreply.github.com>
2022-02-24 23:01:03 +0300

    first commit

---
+++ lib/dice_icons.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class DiceIcons {
+  DiceIcons._();
+
+  static const _kFontFam = 'DiceIcons';
+  static const String? _kFontPkg = null;
+
+  static const IconData dice1 =
+      IconData(0xe800, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData dice2 =
+      IconData(0xe801, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData dice3 =
+      IconData(0xe802, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData dice4 =
+      IconData(0xe803, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData dice5 =
+      IconData(0xe804, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData dice6 =
+      IconData(0xe805, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+}
