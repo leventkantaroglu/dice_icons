9adba236c1217c17310e3ac483bc39a2d0e457f1
Levent Kantaroğlu <63751824+leventkantaroglu@users.noreply.github.com>
2024-03-24 23:24:54 +0300

    Provide documentation

--- lib/dice_icons.dart
+++ lib/dice_icons.dart
@@ -10,11 +10,24 @@ import 'dice_icon_data.dart';
 class DiceIcons {
   DiceIcons._();
 
+  /// 1 dice face
   static const IconData dice1 = DiceIconData(0xe800);
+
+  /// 2 dice face
   static const IconData dice2 = DiceIconData(0xe801);
+
+  /// 3 dice face
   static const IconData dice3 = DiceIconData(0xe802);
+
+  /// 4 dice face
   static const IconData dice4 = DiceIconData(0xe803);
+
+  /// 5 dice face
   static const IconData dice5 = DiceIconData(0xe804);
+
+  /// 6 dice face
   static const IconData dice6 = DiceIconData(0xe805);
+
+  /// empty dice face
   static const IconData dice0 = DiceIconData(0xe806);
 }
